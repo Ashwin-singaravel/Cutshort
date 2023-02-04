6b270247ae0076b9f7f19a6cc4ffe9d3cd996d67
Ashwin <ashwin.s@aximsoft.com>
2023-02-04 23:20:33 +0530

    Database connection moved to config

--- src/config/env/prod.ts
+++ src/config/env/prod.ts
@@ -12,5 +12,11 @@ export const prodSettings: Config = {
         audience: 'https://cutshort-vumw.onrender.com/',
         expiresIn: '12h',
         algorithm: 'RS256'
+    },
+    mongo: {
+        username: process.env.MONGO_DB_USERNAME as string,
+        password: process.env.MONGO_DB_PASSWORD as string,
+        host: process.env.MONGO_DB_HOST as string,
+        db: process.env.MONGO_DB_DEFAULT as string
     }
 }
--- src/types/config.ts
+++ src/types/config.ts
@@ -3,10 +3,18 @@ import { SignOptions } from 'jsonwebtoken';
 export interface Config {
     app: App;
     jwt: SignOptions;
+    mongo: Mongo;
 }
 
 interface App {
     version: string;
     name: string;
     port: number;
+}
+
+interface Mongo {
+    username: string;
+    password: string;
+    host: string;
+    db: string;
 }
